c78dc48206e846028a1944c6462df4e71b20b47b
Raymond Mitchell <raym@torquepowered.com>
2013-10-17 13:59:04 -0700

    Warehouse packs commit.

---
+++ warehousePacks_T3D_Oct2012/Modern_Warehouse_Art_Pack/game/art/shapes/Modern_warehouse/materials.cs
@@ -0,0 +1,578 @@
+
+singleton Material(warehouse_wall_plain_Material__0)
+{
+   mapTo = "Material__0";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Wall_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(wall_corner_tex_wall)
+{
+   mapTo = "tex_wall";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Wall_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(floor_tile_tex_floor)
+{
+   mapTo = "tex_floor";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/floor_tile_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/floor_tile_n";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/floor_tile_s";
+   materialTag0 = "RoadAndPath";
+   pixelSpecular[0] = "1";
+};
+
+singleton Material(ceiling_tile_tex_ceiling)
+{
+   mapTo = "tex_ceiling";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Ceiling_tiles_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "96";
+   translucentBlendOp = "None";
+   materialTag0 = "Warehouse";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Ceiling_tiles_n";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Ceiling_tiles_s";
+   pixelSpecular[0] = "1";
+};
+
+singleton Material(support_beam_ceiling_tex_support_beam)
+{
+   mapTo = "tex_support_beam";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Cap_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(light_fixture_tex_light)
+{
+   mapTo = "tex_light";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Lamp_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(pallet_wood)
+{
+   mapTo = "pallet_wood";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Bench_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(fire_extinguisher_tex_fire_extinguisher)
+{
+   mapTo = "tex_fire_extinguisher";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/fire_extinguisher_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(barrel_tex_barrel)
+{
+   mapTo = "tex_barrel";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/barrel_D";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "56";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Barrel_n";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Barrel_s";
+   pixelSpecular[0] = "1";
+};
+
+singleton Material(cardboard_box_residential_props)
+{
+   mapTo = "residential_props";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/cardboard_box_D";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(crate_pallet_wood0)
+{
+   mapTo = "pallet_wood0";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/crate_D";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(shelf_tex_shelf)
+{
+   mapTo = "tex_shelf";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Shelf_d";
+   specular[0] = "0.996078 0.996078 0.996078 1";
+   specularPower[0] = "8";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Shelf_n";
+   translucent = "0";
+   alphaTest = "1";
+   alphaRef = "100";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Shelf_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(chainlink_fence_tex_fence)
+{
+   mapTo = "tex_fence";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Fence_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "8";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Fence_n";
+   alphaTest = "1";
+   alphaRef = "100";
+   translucent = "1";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Fence_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(concrete_stairs_tex_concrete_stairs)
+{
+   mapTo = "tex_concrete_stairs";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/concrete_stairs_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/concrete_stairs_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/concrete_stairs_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(stairs_tex_metal_stairs)
+{
+   mapTo = "tex_metal_stairs";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Stairs_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "15";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Stairs_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Stairs_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(stairs_tex_metal_stairs)
+{
+   mapTo = "tex_metal_stairs";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Stairs_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "15";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Stairs_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Stairs_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(walkway_02_tex_walkway)
+{
+   mapTo = "tex_walkway";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/walkway_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "25";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/walkway_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/walkway_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(door_tex_structure_props)
+{
+   mapTo = "tex_structure_props";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Structure_Props_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "89";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Structure_Props_s";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Structure_Props_n";
+   materialTag0 = "Warehouse";
+   pixelSpecular[0] = "1";
+   alphaRef = "0";
+};
+
+singleton Material(light_fixture_tex_misc_props)
+{
+   mapTo = "tex_misc_props";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Misc_Props_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "25";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Misc_Props_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Misc_Props_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(light_fixture_tex_misc_props)
+{
+   mapTo = "tex_misc_props";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Misc_Props_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "25";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Misc_Props_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Misc_Props_s";
+};
+
+singleton Material(warehouse_wall_bay_door_Material__62)
+{
+   mapTo = "Material__62";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Bay_Door_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(warehouse_wall_bay_door_tex_bay_door)
+{
+   mapTo = "tex_bay_door";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Bay_Door_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "24";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Bay_Door_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Bay_Door_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(warehouse_wall_bay_door_tex_warehouse_wall_01)
+{
+   mapTo = "tex_warehouse_wall_01";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Wall_01_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_n";
+   pixelSpecular[0] = "0";
+    materialTag0 = "Warehouse";
+};
+
+singleton Material(wall_corner_tex_warehouse_wall_02)
+{
+   mapTo = "tex_warehouse_wall_02";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Wall_02_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(light_fixture_tex_light_bulb)
+{
+   mapTo = "tex_light_bulb";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Light_Bulb_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "11";
+   translucentBlendOp = "None";
+   glow[0] = "1";
+   emissive[0] = "1";
+    materialTag0 = "Warehouse";
+   castShadows = "0";
+};
+
+singleton Material(wall_corner_tex_warehouse_walls)
+{
+   mapTo = "tex_warehouse_walls";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+    glow[0] = "0";
+   emissive[0] = "0";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_n";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(warehouse_wall_ventilation_tex_vent_fan)
+{
+   mapTo = "tex_vent_fan";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Ventilation_Fan_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "16";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Ventilation_Fan_n";
+   materialTag0 = "Warehouse";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Ventilation_Fan_s";
+   alphaTest = "1";
+    alphaRef = "130";
+};
+
+singleton Material(warehouse_wall_ventilation_tex_vent_fan)
+{
+   mapTo = "tex_vent_fan";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Ventilation_Fan_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "16";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Ventilation_Fan_n";
+   alphaTest = "1";
+   alphaRef = "130";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Ventilation_Fan_s";
+   materialTag0 = "RoadAndPath";
+};
+
+singleton Material(warehouse_wall_ventilation_Material__69)
+{
+   mapTo = "Material__69";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_n";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(pillar_tex_ducts_pillars)
+{
+   mapTo = "tex_ducts_pillars";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Ducts_Pillars_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "12";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Ducts_Pillars_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Ducts_Pillars_s";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(pillar_tex_ducts_pillars)
+{
+   mapTo = "tex_ducts_pillars";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Ducts_Pillars_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "12";
+   translucentBlendOp = "None";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Ducts_Pillars_s";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Ducts_Pillars_n";
+};
+
+singleton Material(sprinkler_04_tex_structure_props__sprinkler_four_)
+{
+   mapTo = "tex_structure_props__sprinkler_four_";
+   diffuseMap[0] = "sprinkler_fourAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(sprinkler_02_tex_structure_props__sprinkler_two_)
+{
+   mapTo = "tex_structure_props__sprinkler_two_";
+   diffuseMap[0] = "sprinkler_twoAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(sprinkler_03_tex_structure_props__sprinkler_three_)
+{
+   mapTo = "tex_structure_props__sprinkler_three_";
+   diffuseMap[0] = "sprinkler_threeAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(sprinkler_01_tex_structure_props__sprinkler_one_)
+{
+   mapTo = "tex_structure_props__sprinkler_one_";
+   diffuseMap[0] = "sprinkler_oneAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(pipes_05_tex_structure_props__pipes_five_)
+{
+   mapTo = "tex_structure_props__pipes_five_";
+   diffuseMap[0] = "pipes_fiveAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(pipes_04_tex_structure_props__pipes_four_)
+{
+   mapTo = "tex_structure_props__pipes_four_";
+   diffuseMap[0] = "pipes_fourAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(pipes_03_tex_structure_props__pipes_three_)
+{
+   mapTo = "tex_structure_props__pipes_three_";
+   diffuseMap[0] = "pipes_threeAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(pipes_02_tex_structure_props__pipes_two_)
+{
+   mapTo = "tex_structure_props__pipes_two_";
+   diffuseMap[0] = "pipes_twoAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(pipes_01_tex_structure_props__pipes_one_)
+{
+   mapTo = "tex_structure_props__pipes_one_";
+   diffuseMap[0] = "pipes_oneAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(pipes_06_tex_structure_props__pipes_six_)
+{
+   mapTo = "tex_structure_props__pipes_six_";
+   diffuseMap[0] = "pipes_sixAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(fuse_box_02_tex_structure_props__fuse_box_two_)
+{
+   mapTo = "tex_structure_props__fuse_box_two_";
+   diffuseMap[0] = "fuse_box_twoAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(fuse_box_01_tex_structure_props__fuse_box_one_)
+{
+   mapTo = "tex_structure_props__fuse_box_one_";
+   diffuseMap[0] = "fuse_box_oneAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(support_beam_wall_tex_structure_props__wall_support_beam_)
+{
+   mapTo = "tex_structure_props__wall_support_beam_";
+   diffuseMap[0] = "wall_support_beamAmbient Occlusion _MR_";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(warehouse_wall_window_tex_structure_props_windows)
+{
+   mapTo = "tex_structure_props_windows";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Structure_Props_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   materialTag0 = "Warehouse";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Structure_Props_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Structure_Props_s";
+};
+
+singleton Material(warehouse_wall_window_tex_structure_props_walls)
+{
+   mapTo = "tex_structure_props_walls";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   materialTag0 = "Warehouse";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_n";
+};
+
+singleton Material(door_tex_warehouse_windows)
+{
+   mapTo = "tex_warehouse_windows";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Windows_d";
+   specular[0] = "1 1 1 1";
+   specularPower[0] = "9";
+   materialTag0 = "Warehouse";
+   pixelSpecular[0] = "0";
+   translucent = "1";
+   translucentZWrite = "0";
+};
+
+singleton Material(Crate_Pallet)
+{
+   mapTo = "Crate_Pallet";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Crate_Pallet_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "96";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Crate_Pallet_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/Crate_Pallet_s";
+};
+
+singleton Material(light_fixture_tex_lightFixture)
+{
+   mapTo = "tex_lightFixture";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/LightFixture_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/LightFixture_n";
+   pixelSpecular[0] = "1";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/LightFixture_s";
+   castShadows = "0";
+   materialTag0 = "Warehouse";
+};
+
+singleton Material(floor_tile_01_tex_floor_tile)
+{
+   mapTo = "tex_floor_tile";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/floor_tile_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/floor_tile_n";
+   specularMap[0] = "art/shapes/Modern_warehouse/Textures/floor_tile_s";
+};
+
+singleton Material(support_beam_wall_tex_support_beam00)
+{
+   mapTo = "tex_support_beam00";
+   diffuseMap[0] = "Cap_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(warehouse_wall_plain_tex_modern_warehouse_walls)
+{
+   mapTo = "tex_modern_warehouse_walls";
+   diffuseMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_d";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/Modern_warehouse/Textures/Warehouse_Walls_n";
+   materialTag0 = "Warehouse";
+};
---
+++ warehousePacks_T3D_Oct2012/SciFi_Warehouse_Art_Pack/game/art/shapes/SciFi_warehouse/materials.cs
@@ -0,0 +1,212 @@
+singleton Material(tex_SciFi_walls)
+{
+   mapTo = "tex_SciFi_walls";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Warehouse_Walls_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Warehouse_Walls_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Warehouse_Walls_s.dds";
+};
+
+singleton Material(tex_SciFi_BayDoor)
+{
+   mapTo = "tex_SciFi_BayDoor";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Bay_Door_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Bay_Door_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Bay_Door_s.dds";
+};
+
+singleton Material(tex_SciFi_vent_fan)
+{
+   mapTo = "tex_SciFi_vent_fan";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ventilation_Fan_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ventilation_Fan_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ventilation_Fan_s.dds";
+   translucent = "1";
+   alphaTest = "1";
+};
+
+singleton Material(tex_SciFi_Windows)
+{
+   mapTo = "tex_SciFi_Windows";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Warehouse_Windows_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "LerpAlpha";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Warehouse_Windows_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Warehouse_Windows_s.dds";
+   translucent = "1";
+};
+
+singleton Material(tex_SciFi_Structure_Props)
+{
+   mapTo = "tex_SciFi_Structure_Props";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Structure_Props_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Structure_Props_s.dds";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Structure_Props_n.dds";
+};
+
+singleton Material(tex_SciFi_floor_tile)
+{
+   mapTo = "tex_SciFi_floor_tile";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_floor_tile_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Floor_Tile_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Floor_Tile_s.dds";
+};
+
+singleton Material(tex_SciFi_ceiling)
+{
+   mapTo = "tex_SciFi_ceiling";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ceiling_Tiles_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ceiling_Tiles_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ceiling_Tiles_s.dds";
+};
+
+singleton Material(tex_SciFi_Walkway)
+{
+   mapTo = "tex_SciFi_Walkway";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Walkway_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Walkway_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Walkway_s.dds";
+};
+
+singleton Material(tex_SciFi_Barrel)
+{
+   mapTo = "tex_SciFi_Barrel";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Barrel_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Barrel_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Barrel_s.dds";
+};
+
+singleton Material(tex_SciFi_Crate_Pallet)
+{
+   mapTo = "tex_SciFi_Crate_Pallet";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Crate_Pallet_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Crate_Pallet_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Crate_Pallet_s.dds";
+};
+
+singleton Material(tex_SciFi_Ducts_Pillars)
+{
+   mapTo = "tex_SciFi_Ducts_Pillars";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ducts_Pillars_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ducts_Pillars_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Ducts_Pillars_s.dds";
+};
+
+singleton Material(tex_SciFi_Fence)
+{
+   mapTo = "tex_SciFi_Fence";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Fence_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucent = "1";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Fence_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Fence_d.dds";
+};
+
+singleton Material(tex_SciFi_Misc_Props)
+{
+   mapTo = "tex_SciFi_Misc_Props";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Misc_Props_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Misc_Props_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Misc_Props_s.dds";
+};
+
+singleton Material(tex_SciFi_LightFixture)
+{
+   mapTo = "tex_SciFi_LightFixture";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_LightFixture_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_LightFixture_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_LightFixture_s.dds";
+};
+
+singleton Material(tex_SciFi_outdoor_steps)
+{
+   mapTo = "tex_SciFi_outdoor_steps";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Outdoor_Steps_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Outdoor_Steps_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Outdoor_Steps_s.dds";
+};
+
+singleton Material(tex_SciFi_structure_props)
+{
+   mapTo = "SciFi_structure_props";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Structure_Props_d.dds";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Structure_Props_n.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Structure_Props_s.dds";
+   translucentBlendOp = "None";
+};
+
+singleton Material(tex_SciFi_metal_stairs)
+{
+   mapTo = "tex_SciFi_metal_stairs";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Stairs_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Stairs_n.dds";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Stairs_s.dds";
+};
+
+singleton Material(tex_SciFi_Shelf)
+{
+   mapTo = "tex_SciFi_Shelf";
+   diffuseMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Shelf_d.dds";
+   normalMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Shelf_n.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   specularMap[0] = "art/shapes/SciFi_warehouse/Textures/SciFi_Shelf_s.dds";
+   translucentBlendOp = "None";
+   alphaTest = "1";
+};
+
+singleton Material(tex_SciFi_Light_Bulb)
+{
+   mapTo = "tex_SciFi_Light_Bulb";
+   diffuseMap[0] = "art/shapes/SciFi_Warehouse/Textures/SciFi_Light_Bulb_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   glow[0] = "1";
+   emissive[0] = "1";
+};
