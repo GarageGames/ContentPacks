182b67988d4330f82ee87bc39453884f1fba401d
Raymond Mitchell <raym@torquepowered.com>
2013-10-21 14:05:58 -0700

    Zombie Pack 1.1 Game assets.

---
+++ ZombiePack_T3D1.1_GameAssets/art/shapes/actors/GG_Zombie/materials.cs
@@ -0,0 +1,186 @@
+
+singleton Material(Zombie__5___Default)
+{
+   mapTo = "_5_-_Default";
+   diffuseColor[0] = "0.588235 0.588235 0.588235 1";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+};
+
+singleton Material(zombie_pants_torn)
+{
+   mapTo = "zombie_pants_torn";
+   diffuseMap[0] = "./Textures/zombie_pants_torn_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_pants_torn_n.dds";
+   specularMap[0] = "./Textures/zombie_pants_torn_s.dds";
+};
+
+singleton Material(zombie_head_01)
+{
+   mapTo = "zombie_head_01";
+   diffuseMap[0] = "./Textures/zombie_head_01_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_head_01_n.dds";
+   specularMap[0] = "./Textures/zombie_head_01_s.dds";
+};
+
+singleton Material(zombie__arms)
+{
+   mapTo = "zombie__arms";
+   diffuseMap[0] = "./Textures/zombie_arms_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_arms_n.dds";
+   specularMap[0] = "./Textures/zombie_arms_s.dds";
+};
+
+singleton Material(zombie_hair_05)
+{
+   mapTo = "zombie_hair_05";
+   diffuseMap[0] = "./Textures/zombie_hair_05_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_hair_05_n.dds";
+   specularMap[0] = "./Textures/zombie_hair_05_s.dds";
+};
+
+singleton Material(zombie_tshirt_torn)
+{
+   mapTo = "zombie_tshirt_torn";
+   diffuseMap[0] = "./Textures/zombie_tshirt_torn_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_tshirt_torn_n.dds";
+   specularMap[0] = "./Textures/zombie_tshirt_torn_s.dds";
+};
+
+singleton Material(Zombie_02_zombie_pants)
+{
+   mapTo = "zombie_pants";
+   diffuseMap[0] = "./Textures/zombie_pants_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_pants_n.dds";
+   specularMap[0] = "./Textures/zombie_pants_s.dds";
+};
+
+singleton Material(Zombie_02_zombie_head_02)
+{
+   mapTo = "zombie_head_02";
+   diffuseMap[0] = "./Textures/zombie_head_02_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_head_02_n.dds";
+   specularMap[0] = "./Textures/zombie_head_02_s.dds";
+};
+
+singleton Material(Zombie_02_zombie_hair_01)
+{
+   mapTo = "zombie_hair_01";
+   diffuseMap[0] = "./Textures/zombie_hair_01_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_hair_01_n.dds";
+   specularMap[0] = "./Textures/zombie_hair_01_s.dds";
+   pixelSpecular[0] = "1";
+};
+
+singleton Material(Zombie_02_zombie_tshirt)
+{
+   mapTo = "zombie_tshirt";
+   diffuseMap[0] = "./Textures/zombie_tshirt_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_tshirt_n.dds";
+   specularMap[0] = "./Textures/zombie_tshirt_s.dds";
+};
+
+singleton Material(Zombie_03_zombie_hair_02)
+{
+   mapTo = "zombie_hair_02";
+   diffuseMap[0] = "./Textures/zombie_hair_02_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_hair_02_n.dds";
+   specularMap[0] = "./Textures/zombie_hair_02_s.dds";
+};
+
+singleton Material(Zombie_03_zombie_head_03)
+{
+   mapTo = "zombie_head_03";
+   diffuseMap[0] = "./Textures/zombie_head_03_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_head_03_n.dds";
+   specularMap[0] = "./Textures/zombie_head_03_s.dds";
+};
+
+singleton Material(Zombie_04_zombie_head_04)
+{
+   mapTo = "zombie_head_04";
+   diffuseMap[0] = "./Textures/zombie_head_04_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_head_04_n.dds";
+   specularMap[0] = "./Textures/zombie_head_04_s.dds";
+};
+
+singleton Material(Zombie_04_zombie_hair_03)
+{
+   mapTo = "zombie_hair_03";
+   diffuseMap[0] = "./Textures/zombie_hair_03_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_hair_03_n.dds";
+   specularMap[0] = "./Textures/zombie_hair_03_s.dds";
+};
+
+singleton Material(Zombie_04_zombie_jacket)
+{
+   mapTo = "zombie_jacket";
+   diffuseMap[0] = "./Textures/zombie_jacket_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_jacket_n.dds";
+   specularMap[0] = "./Textures/zombie_jacket_s.dds";
+};
+
+singleton Material(Zombie_05_zombie_hair_04)
+{
+   mapTo = "zombie_hair_04";
+   diffuseMap[0] = "./Textures/zombie_hair_04_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "23";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_hair_04_n.dds";
+   specularMap[0] = "./Textures/zombie_hair_04_s.dds";
+};
+
+singleton Material(Zombie_05_zombie_shirt_long_sleeve)
+{
+   mapTo = "zombie_shirt_long_sleeve";
+   diffuseMap[0] = "./Textures/zombie_shirt_long_d.dds";
+   specular[0] = "0.9 0.9 0.9 1";
+   specularPower[0] = "10";
+   translucentBlendOp = "None";
+   normalMap[0] = "./Textures/zombie_shirt_long_n.dds";
+   specularMap[0] = "./Textures/zombie_shirt_long_s.dds";
+};
